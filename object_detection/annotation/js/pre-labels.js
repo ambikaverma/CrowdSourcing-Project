84ffd23db9b0d34386db2998c991c5b8f5f994f0
Brandon <budang@utexas.edu>
2017-05-01 01:23:00 -0500

    CRLF -> LF

---
+++ object_detection/annotation/js/pre-labels.js
@@ -0,0 +1,30 @@
+var groundtruth = {
+  "144114": [1,3,7],
+  "388902": [63,19,18],
+  "171298": [19,6,5]
+}
+
+var mappings = {
+  1: "person",
+  3: "car",
+  7: "train",
+  5: "airplane",
+  6: "bus",
+  16: "bird",
+  17: "cat",
+  18: "dog",
+  19: "horse",
+  63: "couch",
+  "-1": "NONE_OF_THE_ABOVE",
+  "airplane": 5,
+  "bird": 16,
+  "bus": 6,
+  "car": 3,
+  "cat": 17,
+  "couch": 63,
+  "dog": 18,
+  "horse": 19,
+  "person": 1,
+  "train": 7,
+  "NONE_OF_THE_ABOVE": -1
+};
