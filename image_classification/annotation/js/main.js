a1c116f072ab91c61f72e57df926b48fe80275c5
budang <frogninja@sbcglobal.net>
2018-04-02 11:52:43 -0500

    adding image to 'add object' popup

--- image_classification/annotation/js/main.js
+++ image_classification/annotation/js/main.js
@@ -32,6 +32,7 @@ function prepImg() {
   $(".label").remove();
 
   currentImgSrc = "../../images/" + currentImg + ".jpg";
+  $("img").first().attr("src", currentImgSrc)
   
   $("#counter").text(current + 1);
 
