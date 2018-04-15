1a7246cd3b650b726338a72d63b9819d76a83564
Brandon Dang <frogninja@sbcglobal.net>
2018-04-15 17:02:09 -0500

    adding another filtering condition

--- dataset_creation/vizwiz/vizwiz.py
+++ dataset_creation/vizwiz/vizwiz.py
@@ -4,9 +4,10 @@ import random
 # load VizWiz training annotations
 annotations = json.load(open('/home/dang/datasets/VizWiz/Annotations/train.json'))
 
-# randomly sample 1000 'answerable' images from the training set
-num_samples = 1000
-images = [annotation['image'] for annotation in annotations if annotation['answerable']]
+# randomly sample 100 'answerable' images from the training set
+num_samples = 100
+images = [annotation['image'] for annotation in annotations if \
+          annotation['answerable'] and annotation['answer_type'] != 'unanswerable']
 images = sorted([images[i] for i in random.sample(range(len(images)), num_samples)])
 
 # save list of images to disk
