1d59a829add8ae1cc9a2b3971cfb2847167f61d1
Brandon Dang <frogninja@sbcglobal.net>
2018-04-17 10:42:52 -0500

    selecting fewer images and differently, adding images to repo

--- dataset_creation/vizwiz/vizwiz.py
+++ dataset_creation/vizwiz/vizwiz.py
@@ -1,13 +1,17 @@
 import json
+import os
 import random
 
 # load VizWiz training annotations
-annotations = json.load(open('/home/dang/datasets/VizWiz/Annotations/train.json'))
+# annotations = json.load(open('/home/dang/datasets/VizWiz/Annotations/train.json'))
+
+# get selected subset of VizWiz training images
+images = list(os.walk('subset'))[0][2]
 
 # randomly sample 100 'answerable' images from the training set
 num_samples = 100
-images = [annotation['image'] for annotation in annotations if \
-          annotation['answerable'] and annotation['answer_type'] != 'unanswerable']
+# images = [annotation['image'] for annotation in annotations if \
+#           annotation['answerable'] and annotation['answer_type'] != 'unanswerable']
 images = sorted([images[i] for i in random.sample(range(len(images)), num_samples)])
 
 # save list of images to disk
