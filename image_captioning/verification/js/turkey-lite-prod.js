0d2293ab7922dc9a004a28c04c7d5e07f0a73cc1
budang <frogninja@sbcglobal.net>
2018-07-15 21:18:31 -0700

    removing keystroke logging from captioning HITs

---
+++ image_captioning/verification/js/turkey-lite-prod.js
@@ -0,0 +1,35 @@
+var auditors = {
+  "before_typing_delay": true,
+  "clicks_specific": true,
+  "clicks_total": true,
+  "focus_changes": true,
+  "focus_changes_total": true,
+  "keypresses_specific": false,
+  "keypresses_total": true,
+  "mouse_movement_specific": true,
+  "mouse_movement_total": true,
+  "on_focus_time": true,
+  "pastes_specific": true,
+  "pastes_total": true,
+  "recorded_time_disparity": true,
+  "scrolled_pixels_specific": true,
+  "scrolled_pixels_total": true,
+  "total_task_time": true,
+  "within_typing_delay": true,
+  "url": true,
+  "user_agent": true
+}
+
+/* https://rawgit.com/ */
+var base = "https://cdn.rawgit.com/budang/turkey-lite/61b926c3/js/";
+var promises =
+  [$.getScript(base + "resources/jquery.ba-throttle-debounce.min.js"),
+  $.getScript(base + "resources/visibility_changes.js")];
+
+$.when.apply($, promises).then(function() {
+  for (var name in auditors) {
+    if (auditors[name])
+      $.getScript(base + "auditors/" + name + ".js");
+  }
+});
+
